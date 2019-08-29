124c51c36023974eb5c9e065b87b99c23fbadc48
Kapil Gupta <kpgupta98@gmail.com>
2019-08-29 13:12:17 +0530

    lstm init

---
+++ experiments/lstm_rnn/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from dataloader import *
+from evaluator import *
+from model import *
+from trainer import *
+from visualizer import *
---
+++ experiments/lstm_rnn/lstm_rnn.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import torch
+import torch.nn as nn
+
+class LSTM(nn.module):
+    def __init__(self, config):
+        super(LSTM, self).__init__()
+
+    def forward(self, x):
+        pass
